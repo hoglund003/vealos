1141dc2476da700e78b180bcb4962c77a05b7eb2
Tom Øyvind Hogstad <tom.oyvind.hogstad@gmail.com>
2021-10-05 11:09:48 +0200

    Merge Kasper

--- application/user.py
+++ application/user.py
@@ -1,5 +1,5 @@
 # Elias og Kaseper
-from datetime import date
+from datetime import date, datetime
 from flask import Blueprint
 from flask import flash
 from flask import g
@@ -9,10 +9,66 @@ from flask import request
 from flask import url_for
 from werkzeug.exceptions import abort
 from flask import current_app
+from application.db import get_db
 
 bp = Blueprint("user", __name__)
 
+@bp.route("/user/<int:userId>")
 @bp.route("/user/")
-#@bp.route("/user/<int:user_id>")
-def index():  
-    return render_template("user.html", **locals())
+def index(userId=-1):  
+    userId = str(userId)
+    db = get_db()
+
+    try:
+        # Fetching user, if it exists.
+        user = db.execute('SELECT * FROM users WHERE id = ?', (userId)).fetchone()
+    except:
+        return redirect(url_for('index', user))
+    tours = db.execute('SELECT * FROM tours WHERE user_id = ?', (userId)).fetchall()
+
+    totalTours = 0
+    toursThisYear = 0
+    for tour in tours:
+        totalTours += 1
+
+        tourYear = datetime.strftime(tour['tour_date'], '%Y')
+        currentYear = str(datetime.now().year)
+        if tourYear == currentYear:
+            toursThisYear += 1
+    
+    return render_template("user/user.html", **locals())
+
+@bp.route("/edit-user/<int:userId>")
+def edit(userId=-1):
+    userId = str(userId)
+    db = get_db()
+
+    # Add validation here -->
+    
+    try:
+        user = db.execute('SELECT * FROM users WHERE id = ?', (userId)).fetchone()
+    except:
+        return redirect('/')
+
+    return render_template("user/edit-user.html", **locals())
+
+@bp.route("/update-user/<int:userId>", methods=["POST"])
+def update(userId):
+    userId = str(userId)
+    db = get_db()
+    
+    # And here -->
+
+    updatedUsername = request.form['updated_username']
+    updatedEmail = request.form['updated_email']
+
+    db.execute('''
+                UPDATE users 
+                SET username = ?, 
+                email = ? 
+                WHERE id = ?''',
+                (updatedUsername, updatedEmail, userId)
+                )
+    db.commit()
+
+    return redirect(url_for('user.index', userId=userId))
